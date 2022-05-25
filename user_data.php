c1f5386ca37dac492529b3ca13783400fb693001
Jonathan Wambua <jonathanwambua10219@gmail.com>
2022-05-25 11:45:54 +0300

    export functinality

--- user_data.php
+++ user_data.php
@@ -4,6 +4,9 @@
     </head>
     <body>
         <?php
+
+        ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+
             $name = $_POST["name"];
             $email = $_POST["email"];
             $dob = $_POST["dob"];
@@ -11,6 +14,11 @@
             $country = $_POST["country"];
 
             $data = array("name"=>$name, "email"=>$email, "dob"=>$dob, "gender"=>$gender, "country"=>$country );
+            
+            //Create csv
+            $file = fopen('userdata.csv', 'w');
+            fputcsv($file, $data);
+            fclose($file);
 
             print_r($data);
         ?>
