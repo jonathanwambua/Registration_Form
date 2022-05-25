908a50e43e265a1932514af9f634351bf2f502ea
Jonathan Wambua <jonathanwambua10219@gmail.com>
2022-05-25 11:48:21 +0300

    comment on the error display code

--- user_data.php
+++ user_data.php
@@ -5,7 +5,7 @@
     <body>
         <?php
 
-        ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+        // ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
             $name = $_POST["name"];
             $email = $_POST["email"];
